a0042631c28adf1fe10a60051204cfe0519ed1d5
Thomas Stephenson <ovangle@gmail.com>
2016-05-07 17:14:35 +1000

    Added isCodec, getEncoder and getDecoder functions to codec.
    isCodec tests whether a given object is a codec
    getEncoder and getDecoder retrieve converters from the codec
    while retaining the binding to the codec.

--- src/codec.ts
+++ src/codec.ts
@@ -1,8 +1,28 @@
+import {isFunction, isBlank} from './lang';
 import {Converter, composeConverters, identityConverter} from './converter';
 
 export interface Codec<T,R> {
     encode: Converter<T,R>;
     decode: Converter<R,T>;
+}
+
+export function isCodec(obj: any): boolean {
+    return !isBlank(obj)
+        && isFunction(obj.decode)
+        && isFunction(obj.encode);
+}
+
+export function getEncoder<T,U>(obj: Codec<T,U>): Converter<T,U>{
+    if (!isCodec(obj))
+        throw new TypeError(`object is not a codec: ${obj}`);
+    return obj.encode.bind(obj);
+}
+
+export function getDecoder<T,U>(obj: Codec<T,U>): Converter<U,T> {
+    if (!isCodec(obj))
+        throw new TypeError(`object is not a codec: ${obj}`);
+    return obj.decode.bind(obj);
+
 }
 
 export function composeCodecs<T,U,V>(fst: Codec<T,U>, snd: Codec<U,V>): Codec<T,V> {
--- test/codec.spec.ts
+++ test/codec.spec.ts
@@ -1,11 +1,14 @@
-import {identity, composeCodecs, chainCodecs} from '../src/codec';
-import {success, failure} from '../src/try';
+import {Converter} from '../src/converter';
+import {identity, composeCodecs, chainCodecs, isCodec, Codec, getEncoder, getDecoder} from '../src/codec';
 import {incrementingCodec, toSimpleObjectCodec, intToStringCodec, stringReversingCodec} from "./mock/mock_codecs";
 
 export function codecTests() {
     describe('codec', () => {
         identityCodecTests();
         composeCodecTests();
+        isCodecTests();
+        getEncoderTests();
+        getDecoderTests();
         chainCodecTests();
     });
 }
@@ -24,6 +27,73 @@ function composeCodecTests() {
         var composition = composeCodecs(incrementingCodec, toSimpleObjectCodec);
         expect(composition.encode(41)).toEqual({a: 42});
         expect(composition.decode({a: 43})).toBe(42);
+    });
+}
+
+function isCodecTests() {
+    it('should be able to test whether a given object is a codec', () => {
+        expect(isCodec(null)).toBe(false, 'null is not a codec');
+        expect(isCodec(42)).toBe(false, 'number is not a codec');
+        expect(isCodec({encode: 40, decode: 50})).toBe(false, 'encode and decode are not functions');
+        //Not actually a codec, but matches the interface.
+        expect(isCodec({encode: () => true, decode: () => false}))
+            .toBe(true, 'not blank and encode, decode are both functions');
+        expect(isCodec(identity))
+            .toBe(true, 'identity codec is a codec');
+    })
+}
+
+class IncrementingCodec implements Codec<number,number> {
+    constructor(private incrementBy: number = 1) {}
+    encode(i: number) { return i + this.incrementBy; }
+    decode(i: number) { return i - this.incrementBy; }
+}
+
+
+function getEncoderTests() {
+    describe('getEncoder', () => {
+        it('should be possible to get the encoder from a codec', () => {
+            var encoder = getDecoder(new IncrementingCodec(15));
+            expect(encoder(57)).toBe(42);
+        });
+
+        it('should retain the original binding', () => {
+            class Foo {
+                encoder:Converter<number,number>;
+
+                incrementBy = 15;
+
+                constructor(codec:Codec<number,number>) {
+                    this.encoder = getDecoder(codec);
+                }
+            }
+            var foo = new Foo(new IncrementingCodec(1));
+            expect(foo.encoder(43)).toBe(42);
+        })
+    });
+}
+
+
+function getDecoderTests() {
+    describe('getDecoder', () => {
+        it('should be possible to get the decoder from a codec', () => {
+            var encoder = getEncoder(new IncrementingCodec(15));
+            expect(encoder(27)).toBe(42);
+        });
+
+        it('should retain the original binding', () => {
+            class Foo {
+                decoder:Converter<number,number>;
+
+                incrementBy = 15;
+
+                constructor(codec:Codec<number,number>) {
+                    this.encoder = getEncoder(codec);
+                }
+            }
+            var foo = new Foo(new IncrementingCodec(1));
+            expect(foo.encoder(41)).toBe(42);
+        })
     });
 }
 
